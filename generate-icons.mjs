ffbcf6e062cacb192c0a24e78c0fcdb56ffc4e95
bekboss001 <saparbayevv.01@gmail.com>
2026-06-16 01:08:25 +0500

    feat: update icons from 1MIOSLOGO

--- generate-icons.mjs
+++ generate-icons.mjs
@@ -1,6 +1,6 @@
 import sharp from 'sharp'
 
-const src = 'public/logo.jpg.jpg'
+const src = 'public/1MIOSLOGO.jpg'
 
 for (const size of [192, 512]) {
   await sharp(src)
@@ -9,6 +9,13 @@ for (const size of [192, 512]) {
     .toFile(`public/icon-${size}.png`)
   console.log(`icon-${size}.png done`)
 }
+
+// iOS 180x180
+await sharp(src)
+  .resize(180, 180, { fit: 'contain', background: { r: 255, g: 255, b: 255, alpha: 1 } })
+  .png()
+  .toFile('public/icon-180.png')
+console.log('icon-180.png done')
 
 // favicon 32x32
 await sharp(src)
